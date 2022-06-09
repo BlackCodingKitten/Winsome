88ef5af0d2397c1fedc5b5d85e32b6501e58cb0a
Michela <m.deodati@studenti.unipi.it>
2022-06-09 19:09:53 +0200

    credo di aver finito il client

--- src/WinsomeClientMain.java
+++ src/WinsomeClientMain.java
@@ -38,7 +38,7 @@ public class WinsomeClientMain {
 
     public static ArrayList<String> followerList = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotBoundException {
         // lettura del file config
         System.out.println(
                 "\n**************** Benvenuto in WINSOME ****************\n\nAttendi stiamo recuperando i tuoi dati...");
@@ -66,72 +66,219 @@ public class WinsomeClientMain {
         Socket socket;
         Scanner inputReader = new Scanner(System.in);
         boolean connectionState = true; // true se la connessione è on, false se la connessione è off
+        RmiCallbackInterface server = null;
+        NotifyEventInterface cInterface;
+        String nickname = null;
 
-        //creo e avvio il thread che ascolta le notifiche di aggiornamento del wallet
+        // creo e avvio il thread che ascolta le notifiche di aggiornamento del wallet
         Thread walletNofierThread = new Thread(new WalletRewardNotifier(multicastAddress, multicastPort));
         walletNofierThread.start();
 
-        //tentativo di collegamento con il server
-        //debug.messaggioDiDebug("tentativo di connessione al server");
-        connessionealserver:
-        while(true){
+        // tentativo di collegamento con il server
+        // debug.messaggioDiDebug("tentativo di connessione al server");
+        loop: while (true) {
             try {
                 socket = new Socket(serverAddress, serverPort);
             } catch (IOException e) {
-                //nel caso il serever fosse irraggiungibile o cadesse la connessione è possibile provare a ricollegarsi "Affrontado l'orco"
-                System.out.println("Oh nooo!!!! (x.x)\nSembra che il server sia prigioniero di un grosso e spaventoso orco.\nTi senti coraggioso/a? [S/N]\n Digita S per ritentare, N per uscire");
-                if(SharedMethods.readFromConsole(inputReader).equalsIgnoreCase("N")){
+                // nel caso il serever fosse irraggiungibile o cadesse la connessione è
+                // possibile provare a ricollegarsi "Affrontado l'orco"
+                System.out.println(
+                        "Oh nooo!!!! (x.x)\nSembra che il server sia prigioniero di un grosso e spaventoso orco.\nTi senti coraggioso/a? [S/N]\n Digita S per ritentare, N per uscire");
+                if (SharedMethods.readFromConsole(inputReader).equalsIgnoreCase("N")) {
                     System.out.print("Hai deciso di dartela a gambe. (-^-)");
                     connectionState = false;
-                }else if (SharedMethods.readFromConsole(inputReader).equalsIgnoreCase("S")){
+                } else if (SharedMethods.readFromConsole(inputReader).equalsIgnoreCase("S")) {
                     System.out.println("Tentativo di riconnessione in corso...");
                 }
             }
-        }
-        System.out.println("Congratulazioni "+"\\"+"_(*w*)_/ sei conesso al ServerWinsome.");
-        try{
-            Registry registry= LocateRegistry.getRegistry(serverAddress, serverRmiPort);
-            RmiServiceInterface stub =(RmiServiceInterface) registry.lookup(serverRmiRegistryName);
-        }catch(RemoteException e){
-            e.printStackTrace();
-            return;
-        }
-        String completeRequest = "";
-        String op ="";
-        NotifyEventInterface callback;
-
-        try{
-            /*il printwriter fa parte sempre della classe java.io e estende java.io.Writer,
-             serve a stampare una rappresentazione formattata degli oggetti in un text-output stream, 
-             la classe implementa i metodi di print della classe PrintStream, e non contine metodi per scrivere raw bytes
-             il secondo argomento true serve a settare l'autoFlush, che però verra effettuato solo quando verrano chiamati metodi di print
-             i metodi della classe PrintWriter non generano IOExceptions */
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while(true){
-                //Ricevo il comando dall'utente se è vuoto lo ignoro
-                completeRequest = SharedMethods.readFromConsole( inputReader);
-                if(completeRequest.equals("")){
-                    continue;
-                }
-                String[] splitCommandLine = completeRequest.split(" ");
-                op = splitCommandLine[0]; // il primo elemnto dell'array splitCommandLine è l'operazione da eseguire
-                //copio il resto degli argomenti in un'altro array
-                String[] otherArgumentsInCommandLine = new String[splitCommandLine.length -1];
-                System.arraycopy(splitCommandLine, 1, otherArgumentsInCommandLine, 0, (splitCommandLine.length-1));
-                //effettuo la switch su op per vedere qual'è l'operazione richiesta dall'utente
-                switch(op){
-                    case "help":
-                    help();
-                        break;
-                }
+            System.out.println("Congratulazioni " + "\\" + "_(*w*)_/ sei conesso al ServerWinsome.");
+            RmiServiceInterface stub;
+            try {
+                Registry registry = LocateRegistry.getRegistry(serverAddress, serverRmiPort);
+                stub = (RmiServiceInterface) registry.lookup(serverRmiRegistryName);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                return;
             }
-        }catch(){
+            String completeRequest = "";
+            String op = "";
+            NotifyEventInterface callback = null;
+            String fromServer;
+            boolean successRequest = true;//true se la request è eseguita correttamente false se la request fallisce
 
-        }
+            try {
+                /*
+                 * il printwriter fa parte sempre della classe java.io e estende java.io.Writer,
+                 * serve a stampare una rappresentazione formattata degli oggetti in un
+                 * text-output stream,
+                 * la classe implementa i metodi di print della classe PrintStream, e non
+                 * contine metodi per scrivere raw bytes
+                 * il secondo argomento true serve a settare l'autoFlush, che però verra
+                 * effettuato solo quando verrano chiamati metodi di print
+                 * i metodi della classe PrintWriter non generano IOExceptions
+                 */
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while (true) {
+                    // Ricevo il comando dall'utente se è vuoto lo ignoro
+                    System.out.println("WinsomeServer è in attesa di istruzioni...");
+                    if(successRequest == false){
+                        //leggo l'input utente e lo ignoro
+                        SharedMethods.readFromConsole( inputReader);
+                        if(callback!=null){
+                            //se non ci fosse questo controllo il client terminerebbe 
+                            UnicastRemoteObject.unexportObject(callback, false);
+                        }
+                        successRequest = true;
+                    }else{
+                        completeRequest = SharedMethods.readFromConsole(inputReader);
+                        debug.messaggioDiDebug("Richiesta fatta al server: " + completeRequest);
+                        if (completeRequest.equals("")) {
+                            continue;
+                        }
+                    }
+                    
 
+                    String[] splitCommandLine = completeRequest.split(" ");
+                    op = splitCommandLine[0]; // il primo elemnto dell'array splitCommandLine è l'operazione da eseguire
+                    // copio il resto degli argomenti in un'altro array
+                    String[] otherArgumentsInCommandLine = new String[splitCommandLine.length - 1];
+                    System.arraycopy(splitCommandLine, 1, otherArgumentsInCommandLine, 0,
+                            (splitCommandLine.length - 1));
+                    // effettuo la switch su op per vedere qual'è l'operazione richiesta dall'utente
+                    switch (op) {
+                        case "help":
+                            help();
+                            break;
+                        case "logout":
+                            // operazione di logout
+                            SharedMethods.sendToServer(out, completeRequest);
+                            fromServer = SharedMethods.readFromServer(in);
+                            // se il logout ha avuto successo lato sever
+                            if (fromServer.equalsIgnoreCase("OK")) {
+                                System.out.println("Logout avvenuto con successo.\nArrivederci!");
+                                server.callbackUnregister(nickname);
+                                UnicastRemoteObject.unexportObject(callback, false);
+                                callback = null;
+                            } else {
+                                // Se il logout non ha avuto successo:
+                                System.out.println(
+                                        "Impossibile effettuare il logout, voglio tenerti qui con me per sempre\nMUAHAHAHA }:-) ");
+                            }
+                            break;
+                        case "login":
+                            // operazione di login
+                            SharedMethods.sendToServer(out, completeRequest);
+                            fromServer = SharedMethods.readFromServer(in);
+                            if (fromServer.equalsIgnoreCase("OK")) {
+                                nickname = otherArgumentsInCommandLine[0];
+                                // registrazione al servizio notifiche per la lista follower
 
-    }
+                                Registry callbackRegistry = LocateRegistry.getRegistry(serverAddress, clientRmiPort);
+                                server = (RmiCallbackInterface) callbackRegistry.lookup(serverRmiCallbackRegistryName);
+                                callback = new NotifyEvent();
+                                cInterface = (NotifyEventInterface) UnicastRemoteObject.exportObject(callback, 0);
+
+                                server.callbackRegister(nickname, cInterface);
+                                followerList = stub.followerList(nickname, otherArgumentsInCommandLine[1]);
+                                System.out.println("Bentornato nel magico mondo di Winsome " + nickname
+                                        + " siamo lieti di rivederti.");
+
+                            } else {
+                                System.out.println("Ops, sembra che qualcosa sia andato storto (TT.TT) " + fromServer);
+                            }
+                            break;
+                        case "forcedexit":
+                            // operazione di uscita forzata
+                            socket.close();
+                            out.close();
+                            in.close();
+                            break loop;
+                        case "register":
+                            // operazione di register
+                            // per prima cosa controllo che siano stati inseriti nome utente, password e max
+                            // 5 tag
+                            if (otherArgumentsInCommandLine.length > 7) {
+                                System.out.println(
+                                        "Impossibile eseguire la registrazione di un nuovo utente.\nPer informazioni su come eseguire una corretta registrazione digita \"help\".");
+                                break;
+                            } else {
+                                // copio la lista dei tag dall'array otherArgumentInCommandLine
+                                ArrayList<String> tags = new ArrayList<>(Arrays.asList(otherArgumentsInCommandLine)
+                                        .subList(2, (otherArgumentsInCommandLine.length - 1)));
+                                boolean flag = stub.registerNewUser(otherArgumentsInCommandLine[0],
+                                        otherArgumentsInCommandLine[1], tags);
+                                if (flag) {
+                                    System.out.println("Congratulazioni " + otherArgumentsInCommandLine[0]
+                                            + " la tua registrazione ha avuto successo.");
+                                } else {
+                                    System.out.println(
+                                            "Siamo spiacenti non è possibile concludere la fase di registrazione.\nRitenta cambiando username");
+                                }
+                                break;
+                            }
+                        case "listfollowers":
+                            // operazione gestita dal client perchè la lista dei follower è salvata in
+                            // locale
+                            if (nickname != null) {
+                                if (followerList.size() > 0) {
+                                    System.out.println("Lista dei tuoi followers:");
+                                    for (String follower : followerList) {
+                                        System.out.println(follower);
+                                        break;
+                                    }
+                                } else {
+                                    System.out.println("Nessuno ti segue. Sei nuovo o sei solo antipatico?\n****** "
+                                            + "\\" + "_(a.a)_/ ******");
+                                    break;
+                                }
+                            } else {
+                                System.out.println(
+                                        "Non posso sapere chi ti segue se non so chi sei, effettua il login prima.");
+                                break;
+                            }
+                        case "listusers":
+                        case "listfollowing":
+                        case "blog":
+                        case "showfeed":
+                        case "wallet":
+                        case "walletbtc":
+                        case "follow":
+                        case "unfollow":
+                        case "rewin":
+                        case "rate":
+                        case "showpost":
+                        case "comment":
+                        case "delete":
+                        case "post":
+                            // tutte queste altre operazioni vengono gestite direttamente dal server senza
+                            // bisogno di ulteriori controlli
+                            SharedMethods.sendToServer(out, completeRequest);
+                            System.out.println("WinsomeServer: " + SharedMethods.readFromServer(in));
+                            break;
+                        default:
+                            // nessun comando riconosciuto
+                            System.out.println("Operazione non riconosciuta, per favore riprova.");
+                            SharedMethods.readFromConsole(inputReader);
+
+                    }// end switch
+
+                } // end while richieste utente
+            } catch (IOException e) {
+                // persa la connessione col server
+                System.out.println("Oh no l'orco cattivo è tornato e si è preso il nostro server.\nVuoi affrontarlo di nuovo? [S/N]")
+                if(SharedMethods.readFromConsole(inputReader).equalsIgnoreCase("S")){
+                    if(op.equalsIgnoreCase("login") ||op.equalsIgnoreCase("listfollowers")|| op.equalsIgnoreCase("register") || op.equalsIgnoreCase("logout")||op.equalsIgnoreCase("forcedexit"));
+                    successRequest = false;
+                    //in questo modo se l'ultima richiesta è login o register o logout o listfollowers o un'uscita forzata viene ricopiata direttamente senza che l'utente debba riscrivere il comando
+                }else{
+                    connectionState = false;
+                    System.out.println("Le nostre armate sono dalla tua parte.");
+                    break;
+                }
+            } // end catch
+        } // end loop
+    }// end main
 
     // recupera l'indirizzo dal file di config
     public static void setServerAddress() {
@@ -190,6 +337,8 @@ public class WinsomeClientMain {
         System.out.println("Hai bidogno di aiuto?\nEccoti una clista dei comadi pronta per te:");
         System.out.println("help    Serve a mostrare questa lista, ma questo lo sai. :-)");
         System.out
+                .println("register <username> <password> <elnenca max 5 tag>       Serve per registrare nuovi utenti");
+        System.out
                 .println("login <username> <password>   Serve per effettuare il login nel magico mondo di Winsome.");
         System.out.println("logout   Serve per sloggare dal magico mondo di Winsome. :-(");
         System.out.println(
@@ -209,6 +358,7 @@ public class WinsomeClientMain {
         System.out.println("post \"<titolo>\" \"<testo>\"   Serve per creare un post.");
         System.out.println("delete <idpost>  Serve per eliminare un post.");
         System.out.println("comment <idpost> <testo del commento> Serve per commentare un post.");
+        System.out.println("forcedexit      Serve per forzare l'uscita dal social");
     }
 
     // metodo per la stampa della barra di caricamento
